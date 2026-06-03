4f52e023ca4afb60cdbefbce971c268294a318a7
John Whalen <jwhalen323@gmail.com>
2026-06-03 17:46:56 -0400

    update init to account for previous commit

--- src/piano_modeling/__init__.py
+++ src/piano_modeling/__init__.py
@@ -4,3 +4,4 @@ from .common import DEVICE, get_device, setup_runtime
 from .paths import ProjectPaths, make_colab_paths, print_paths
 from .models import PianoTranscriptionSystem, count_parameters_millions
 from .training import load_checkpoint, run_training, save_checkpoint, train_one_epoch, validate_quick
+from .lr_finder import find_optimal_lr_fastai
